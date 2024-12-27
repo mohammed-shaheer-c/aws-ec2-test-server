524397bc08337b0b9472cfc65db296094ba95254
Mohammed-Shaheer-1 <shaheershan3@gmail.com>
2024-12-27 22:49:37 +0530

    second commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (!fs.existsSync(uploadDir)) {
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/'); // Save files in the 'uploads' directory
-  },
+  }, 
   filename: (req, file, cb) => {
     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
     cb(null, `${uniqueSuffix}-${file.originalname}`); // Save files with a unique name
